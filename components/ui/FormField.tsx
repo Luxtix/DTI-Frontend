34bd32a988fd8766d506133ca0cf592189314fa7
Jacob Irwanto <jacobirwanto@gmail.com>
2024-06-24 15:40:07 +0700

    style(jacob) change form border color

--- components/ui/FormField.tsx
+++ components/ui/FormField.tsx
@@ -7,11 +7,11 @@ interface FormFieldProps {
 function FormField({ label, type, placeholder }: FormFieldProps) {
   return (
     <div className="mb-4">
-      <label className="block text-zinc-700">{label}</label>
+      <label className="block text-luxtix-8">{label}</label>
       <input
         type={type}
         placeholder={placeholder}
-        className="w-full border rounded-lg p-2"
+        className="w-full border rounded-lg p-2 focus:border-luxtix-5 focus:outline-none"
       />
     </div>
   );
