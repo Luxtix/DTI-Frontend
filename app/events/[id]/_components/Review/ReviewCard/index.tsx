d0c9f4a4c57b8b70adf3e38cc4978c2a5edb9696
Jacob Irwanto <jacobirwanto@gmail.com>
2024-07-05 16:38:56 +0700

    feat(jacob) add dashboard, change review component

--- app/events/[id]/_components/Review/ReviewCard/index.tsx
+++ app/events/[id]/_components/Review/ReviewCard/index.tsx
@@ -1,5 +1,4 @@
 import { ReviewType } from "@/types/review";
-import Image from "next/image";
 
 interface ReviewProps {
   review: ReviewType;
@@ -7,35 +6,21 @@ interface ReviewProps {
 
 function ReviewCard({ review }: ReviewProps) {
   return (
-    <div className="flex flex-col space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0 bg-white p-4 rounded-lg">
-      <Image
-        src={review.profile}
-        alt="Reviewer image"
-        className="w-12 h-12 rounded-full"
-        width={48}
-        height={48}
-      />
-      <div className="flex-1">
-        <div className="flex flex-col sm:flex-row sm:space-x-2 items-center">
-          <span className="font-bold">{review.name}</span>
-          <span className="text-xs font-extralight text-luxtix-8">
-            wrote a review for
-          </span>
-          <span className="font-light text-luxtix-5">{review.title}</span>
-        </div>
-        <p className="text-sm">{review.reviewText}</p>
+    <div className="grid grid-cols-1 sm:grid-cols-5 gap-4 p-4 border-b sm:text-center bg-white">
+      <div className="col-span-1 text-sm sm:text-base break-words">
+        {review.number}
       </div>
-      <div className="flex items-center space-x-1">
-        {Array.from({ length: 5 }, (_, index) => (
-          <span
-            key={index}
-            className={
-              index < review.stars ? "text-yellow-500" : "text-luxtix-7"
-            }
-          >
-            ★
-          </span>
-        ))}
+      <div className="col-span-1 text-sm sm:text-base break-words">
+        {review.name}
+      </div>
+      <div className="col-span-1 text-sm sm:text-base break-words">
+        {review.title}
+      </div>
+      <div className="col-span-1 text-sm sm:text-base break-words">
+        {review.reviewText}
+      </div>
+      <div className="col-span-1 text-sm sm:text-base break-words">
+        {`${review.stars}/5`}
       </div>
     </div>
   );
--- app/events/[id]/_components/Review/index.tsx
+++ app/events/[id]/_components/Review/index.tsx
@@ -1,5 +1,4 @@
-import { AiOutlineArrowRight } from "react-icons/ai";
-import { AiOutlineArrowLeft } from "react-icons/ai";
+import { AiOutlineArrowRight, AiOutlineArrowLeft } from "react-icons/ai";
 import reviews from "@/utils/reviewItems";
 import ReviewCard from "./ReviewCard";
 
@@ -7,20 +6,26 @@ function Review() {
   return (
     <div className="max-w-7xl mx-auto px-4 py-4 pb-8">
       <h2 className="text-lg font-semibold text-luxtix-5 mb-4">Reviews</h2>
-      <div className="space-y-4">
+      <div className="hidden sm:grid grid-cols-5 gap-4 p-4 bg-secondary text-secondary-foreground font-semibold sm:text-center">
+        <div className="col-span-1">#</div>
+        <div className="col-span-1">Name</div>
+        <div className="col-span-1">Subject</div>
+        <div className="col-span-1">Review</div>
+        <div className="col-span-1">Rating</div>
+      </div>
+      <div>
         {reviews.map((review, index) => (
           <ReviewCard key={index} review={review} />
         ))}
       </div>
-      <div className="flex justify-between items-center">
-        <div className="flex flex-row mt-4 space-x-8">
-          <button>
-            <AiOutlineArrowLeft size={20} className="btn-anim text-luxtix-1" />
-          </button>
-          <button>
-            <AiOutlineArrowRight size={20} className="btn-anim text-luxtix-1" />
-          </button>
-        </div>
+
+      <div className="flex justify-between items-center mt-4">
+        <button>
+          <AiOutlineArrowLeft size={25} className="btn-anim text-luxtix-1" />
+        </button>
+        <button>
+          <AiOutlineArrowRight size={25} className="btn-anim text-luxtix-1" />
+        </button>
       </div>
     </div>
   );
