86ca852ebac9a98723368b6a0a43b6077f00054e
akaslayer <hendrysurijanto123@gmail.com>
2024-07-16 03:32:52 +0700

    Feat(Transaction):Feature for transaction is halfaway done

--- auth.ts
+++ auth.ts
@@ -1,6 +1,6 @@
-import NextAuth from "next-auth";
-import Credentials from "next-auth/providers/credentials";
-import { cookies } from "next/headers";
+import NextAuth from 'next-auth'
+import Credentials from 'next-auth/providers/credentials'
+import { cookies } from 'next/headers'
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   secret: process.env.NEXT_PUBLIC_SECRET,
@@ -8,34 +8,34 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Credentials({
       credentials: {
-        email: { label: "email", type: "text" },
-        password: { label: "password", type: "password" },
+        email: { label: 'email', type: 'text' },
+        password: { label: 'password', type: 'password' },
       },
       authorize: async (credentials) => {
         try {
           const response = await fetch(`http://localhost:8080/api/auth/login`, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
               email: credentials.email,
               password: credentials.password,
             }),
-            credentials: "include",
-          });
+            credentials: 'include',
+          })
 
           if (!response.ok) {
-            throw new Error("Failed to log in");
+            throw new Error('Failed to log in')
           }
 
-          const data = await response.json();
+          const data = await response.json()
 
-          const useCookies = cookies();
-          useCookies.set("Sid", data.accessToken, {
+          const useCookies = cookies()
+          useCookies.set('Sid', data.accessToken, {
             httpOnly: true,
             secure: false,
             maxAge: 6000,
-            path: "/",
-          });
+            path: '/',
+          })
 
           return {
             id: data.id,
@@ -43,37 +43,38 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
             sub: data.email,
             role: data.role,
             accessToken: data.accessToken,
-          };
+          }
         } catch (error) {
-          console.log(error);
-          return null;
+          console.log(error)
+          return null
         }
       },
     }),
   ],
   callbacks: {
     async authorized({ auth }) {
-      return !!auth;
+      return !!auth
     },
     async session({ token, session }) {
-      if (token.email) session.user.email = token.email;
-      if (token.role) session.user.role = token.role;
-      return session;
+      if (token.email) session.user.email = token.email
+      if (token.role) session.user.role = token.role
+
+      return session
     },
     async jwt({ token, user }) {
       if (user && user.email) {
-        token.sub = user.email;
-        token.email = user.email;
+        token.sub = user.email
+        token.email = user.email
       }
       if (user && user.role) {
-        token.role = user.role;
+        token.role = user.role
       }
-      return token;
+      return token
     },
   },
-  session: { strategy: "jwt", maxAge: 60 * 60 * 1 },
+  session: { strategy: 'jwt', maxAge: 60 * 60 * 1 },
   pages: {
-    signIn: "/sign-in",
+    signIn: '/sign-in',
   },
   jwt: {
     maxAge: 60 * 60 * 1,
@@ -83,8 +84,8 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       name: `session-jwt`,
       options: {
         httpOnly: true,
-        sameSite: "lax",
+        sameSite: 'lax',
       },
     },
   },
-});
+})
