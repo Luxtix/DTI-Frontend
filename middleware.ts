a3d9a4355c4e0ad676f9494605630b369a89801f
Jacob Irwanto <jacobirwanto@gmail.com>
2024-07-17 23:23:19 +0700

    fix(jacob) limit size fix on landing page

--- middleware.ts
+++ middleware.ts
@@ -1,61 +1,61 @@
-import { auth } from '@/auth'
-import { NextResponse } from 'next/server'
+import { auth } from "@/auth";
+import { NextResponse } from "next/server";
 
 export const config = {
   matcher: [
-    '/',
-    '/events',
-    '/events/:id',
-    '/events/:id/transactions',
-    '/dashboard',
-    '/create-event',
-    '/profile',
-    '/order-success',
-    '/interested',
-    '/sign-in',
-    '/sign-up',
-    '/purchased-tickets',
+    "/",
+    "/events",
+    "/events/:id",
+    "/events/:id/transactions",
+    "/dashboard",
+    "/create-event",
+    "/profile",
+    "/order-success",
+    "/interested",
+    "/sign-in",
+    "/sign-up",
+    "/purchased-tickets",
   ],
-}
+};
 export default auth((req) => {
-  const reqUrl = new URL(req.url)
-  const path = reqUrl.pathname
+  const reqUrl = new URL(req.url);
+  const path = reqUrl.pathname;
 
-  console.log(req.auth?.user.role)
-  const publicRoutes = ['/', '/events', '/sign-in', '/sign-up']
-  if (publicRoutes.includes(path) || path.startsWith('/events/')) {
-    return NextResponse.next()
+  console.log(req.auth?.user.role);
+  const publicRoutes = ["/", "/events", "/sign-in", "/sign-up"];
+  if (publicRoutes.includes(path) || path.startsWith("/events/")) {
+    return NextResponse.next();
   }
 
   if (!req.auth) {
-    return NextResponse.redirect(new URL('/sign-in', req.url))
+    return NextResponse.redirect(new URL("/sign-in", req.url));
   }
 
-  const { role } = req.auth.user
+  const { role } = req.auth.user;
 
-  if (role === 'ORGANIZER') {
-    const organizerRoutes = ['/dashboard', '/create-event', '/profile']
+  if (role === "ORGANIZER") {
+    const organizerRoutes = ["/dashboard", "/create-event", "/profile"];
     if (!organizerRoutes.includes(path)) {
-      return NextResponse.redirect(new URL('/dashboard', req.url))
+      return NextResponse.redirect(new URL("/dashboard", req.url));
     }
   }
 
-  if (role === 'USER') {
+  if (role === "USER") {
     const userRoutes = [
-      '/',
-      '/events',
-      '/profile',
-      '/order-success',
-      '/interested',
-      '/purchased-tickets',
-    ]
+      "/",
+      "/events",
+      "/profile",
+      "/order-success",
+      "/interested",
+      "/purchased-tickets",
+    ];
     if (
       !userRoutes.includes(path) &&
       !path.match(/^\/events\/[^/]+$/) &&
       !path.match(/^\/events\/[^/]+\/transactions$/)
     ) {
-      return NextResponse.redirect(new URL('/', req.url))
+      return NextResponse.redirect(new URL("/", req.url));
     }
   }
-  return NextResponse.next()
-})
+  return NextResponse.next();
+});
